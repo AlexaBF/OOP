b4610308e543776d5735341fda1d95d46698180e
AlexaBF <A01422793@tec.mx>
2023-01-30 00:00:29 -0600

    upgrade

--- Proyecto/Vuelo.h
+++ Proyecto/Vuelo.h
@@ -2,15 +2,13 @@
 
 #include <iostream>
 #include <string>
-#include "Fecha.h"
 
 using namespace std;
 
 class Vuelo{
 private:
-    string noVuelo,nombre, destino, duracionViaje,detallesAvion,detallesPasajeros;
+    string noVuelo,nombre, destino, duracionViaje,detallesAvion,detallesPasajeros,fecha;
     int precio,km;
-    Fecha fecha;
 public:
     Vuelo(/* args */);
     string getFecha();
