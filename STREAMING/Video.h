6b1a83cb62342e26dd428dd8362f776e05d2205f
AlexaBF <57883150+AlexaBF@users.noreply.github.com>
2024-11-12 12:55:45 -0600

    Update Video.h

--- STREAMING/Video.h
+++ STREAMING/Video.h
@@ -3,10 +3,10 @@
 #ifndef Video_H
 #define Video_H
 
-#include <stdio.h>
 #include <iostream>
 #include <string>
 using namespace std;
+
 class Video{
 private: //atributos
     
@@ -21,4 +21,39 @@ public: //metodos
     void mostrarVideo();
     virtual ~Video();
 };
-#endif 
+
+
+
+Video::Video(int _tipo,int _id,string _nombre,string _genero,float _duracion){
+    tipo=_tipo;
+    id=_id;
+    nombre=_nombre;
+    genero=_genero;
+    duracion=_duracion;
+}
+
+
+void Video::leerArchivo(){
+    FILE *fp;
+    int c;
+    fp=fopen("Videos.txt","r");
+    while (1) {
+        c=fgetc(fp);
+        if (feof(fp)) {
+            break;
+        }
+        printf("%c",c);
+    }
+    fclose(fp);
+}
+
+
+void Video::mostrarVideo(){
+    cout<<"ID: "<<id<<" | Tipo: "<<tipo<<" | Nombre: "<<nombre<<" | Género: "<<genero<<" | Duración: "<<duracion<<" min | "<<endl;
+}
+
+
+Video::~Video(){
+}
+
+#endif
